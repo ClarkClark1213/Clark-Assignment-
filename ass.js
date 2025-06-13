5684a443e3904863a82e1ed32171d86fdc7c7b82
ClarkClark1213 <Clarkjohnbucog13@gmail.com>
2025-06-13 23:09:13 +0800

    Update ass.js

--- ass.js
+++ ass.js
@@ -9,58 +9,120 @@ let Organization = document.getElementById("Organization");
 let Street = document.getElementById("Street");
 let Email = document.getElementById("Email");
 
+
+
 function submitclick() {  
-    
-    
+
+
     if(a.value.trim() === ""){
-        alert("Enter first name"); 
+        alert("Enter first name");
+        a.style.boxShadow = " 0px 0px 10px 5px red"
         return false;
+    }
+    else{
+        a.style.boxShadow = ""
     }
     if(b.value.trim() === ""){
         alert("Enter last name");
+        b.style.boxShadow = " 0px 0px 10px 5px red"
         return false;
+    }
+    else{
+        b.style.boxShadow = ""
     }
     if(a.value === b.value){
         alert("First name and last name are the same!");
+        a.style.boxShadow = " 0px 0px 10px 5px red"
+        b.style.boxShadow = " 0px 0px 10px 5px red"
         return false;
+    }
+    else{
+        a.style.boxShadow = ""
+        b.style.boxShadow = ""                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+        
     }
     if(t.value.trim() === ""){
         alert("Enter telephone number");
+        t.style.boxShadow = " 0px 0px 10px 5px red"
         return false;
+    }
+    else{
+        t.style.boxShadow = ""
     }
     if(isNaN(t.value)){
         alert("Please enter a number in Telephone number");
+        t.style.boxShadow = " 0px 0px 10px 5px red"
         return false;
     }
-    if(Province.value.trim() !== "Davao del Norte"){
-        alert("Select Davao del Norte in Province"); 
-        return false;
-    }
-    if(City.value.trim() !== "Tagum City"){
-        alert("Select Tagum City in City");
-        return false;
-    }
-    if(Barangay.value.trim() !== "Mankilam"){
-        alert("Select Mankilam Barangay"); 
-        return false;
-    }
-    if(Organization.value.trim() === ""){  
-        alert("Enter Organization"); 
-        return false;
-    }
-    if(Street.value.trim() === ""){
-        alert("Enter Street");
-        return false;
+    else{
+        t.style.boxShadow = ""
     }
     if(Email.value.trim() === ""){
         alert("Enter Email"); 
+        Email.style.boxShadow = " 0px 0px 10px 5px red" 
         return false;
     }
-    if(Message.value.length < 10){
-        alert("Message too short");
+    else{
+        Email.style.boxShadow = "" 
+    }
+    if(Organization.value.trim() === ""){  
+        alert("Enter Organization"); 
+        Organization.style.boxShadow = " 0px 0px 10px 5px red" 
         return false;
+    }
+    else{
+        Organization.style.boxShadow = "" 
+    }
+    if(Province.value.trim() !== "Davao del Norte"){
+        alert("Select Davao del Norte in Province");
+        Province.style.boxShadow = " 0px 0px 10px 5px red" 
+        return false;
+    }
+    else{
+        Province.style.boxShadow = ""
+    }
+    if(City.value.trim() !== "Tagum City"){
+        alert("Select Tagum City in City");
+        City.style.boxShadow = " 0px 0px 10px 5px red" 
+        return false;
+    }
+    else{
+        City.style.boxShadow = ""
+    }
+    if(Barangay.value.trim() !== "Mankilam"){
+        alert("Select Mankilam Barangay"); 
+        Barangay.style.boxShadow = " 0px 0px 10px 5px red" 
+        return false;
+    }
+    else{
+        Barangay.style.boxShadow = ""
     }
     
+    
+    if(Street.value.trim() === ""){
+        alert("Enter Street");
+        Street.style.boxShadow = " 0px 0px 10px 5px red" 
+        return false;
+    }
+    else{
+        Street.style.boxShadow = ""
+    }
+    if(Message.value.trim() === ""){
+        alert("fill message");
+        Message.style.boxShadow = " 0px 0px 10px 5px red" 
+        return false;
+    }
+    else{
+        Message.style.boxShadow = ""
+    }
+    if(Message.value.length < 10){
+        alert("Message too shorts");
+        Message.style.boxShadow = " 0px 0px 10px 5px red" 
+        return false;
+    }
+    else{
+        Message.style.boxShadow = ""
+    }
     alert("Submitted Successfully");
     return true;
 }
